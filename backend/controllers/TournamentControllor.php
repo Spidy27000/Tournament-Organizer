4fd04be532d202973782d4f8b7413d4272d8ae93
Tanish Solanki <tanishs4313@gmail.com>
2024-11-11 01:15:00 +0530

    more routes added

--- backend/controllers/TournamentControllor.php
+++ backend/controllers/TournamentControllor.php
@@ -51,20 +51,137 @@ class TournamentControllor
         $username = $data['username'] ?? null;
         $members = $data['members'] ?? null;
         $userModel = new UserModel();
+        $res = self::$tournamentModel->tournamentExists($id);
+        if ($res){
+            echo json_encode([
+                'status' => 'Failed',
+                "error" => 'Tournament not Found'
+            ]);
+        }
         
         $tournamentType = self::$tournamentModel->getTournamentType($id);
 
         if($tournamentType == "Single"){
             $userId = $userModel->getUserId($username);
-            self::$tournamentModel->addPlayer($id, $userId);
+            $res = self::$tournamentModel->addPlayer($id, $userId);
         }else{
             $membersId = [];
             foreach ($members as $el){
                 $temp = $userModel->getUserId($el);
                 array_push($membersId, $temp);
             }
-            self::$tournamentModel->addTeam($id,$teamId, $membersId);
+            $res= self::$tournamentModel->addTeam($id,$teamId, $membersId);
         }
+
+        echo json_encode([
+            'status' => 'success',
+            'id' => $res
+        ]);
+
+    }
+    
+    ///leave/tournament
+    public static function leave(){
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        
+        $teamId = $data['teamId'] ?? null;
+        
+        $tournamentType = self::$tournamentModel->getTournamentType($teamId);
+
+        if($tournamentType == "Single"){
+            self::$tournamentModel->leavePlayer($teamId);
+        }else{
+            self::$tournamentModel->leaveTeam($teamId);
+        }
+        echo json_encode([
+            'status' => 'success'
+        ]);
+    }
+    ///view/tournament/{id:\d+}
+    public static function view($id){
+        // {
+        //     "name" : "",
+        //     "max_size" : "",
+        //     "publicOrPrivate" :"",
+        //     "status" : "",
+        //     "currentCount": "",
+        //     "SingleOrTeam" : ""
+        // }
+        $id = $id['id'];
+        $res = [];
+        $res['name'] = self::$tournamentModel->getName($id);
+        $res['max_size'] = self::$tournamentModel->getMaxSize($id);
+        $res['visibility'] = self::$tournamentModel->getVisibilty($id);
+        $res['status'] = self::$tournamentModel->getStatus($id);
+        $res['current_count'] = self::$tournamentModel->getCurrentCount($id);
+        $res['type'] = self::$tournamentModel->getTournamentType($id);
+        echo json_encode($res);
+
+    }
+
+    ///view/mytournaments/{id:\d+}
+    public static function viewMyTornaments($id){
+
+
+    }
+
+
+    ///view/allTournament
+    public static function viewAllTournaments(){
+        $ids = self::$tournamentModel->getAllTournamentIds();
+        $res = [];
+        $i = 0;
+        foreach ($ids as $_ => $id){
+            $_res = [];
+            $_res['name'] = self::$tournamentModel->getName($id);
+            $_res['max_size'] = self::$tournamentModel->getMaxSize($id);
+            $_res['visibility'] = self::$tournamentModel->getVisibilty($id);
+            $_res['status'] = self::$tournamentModel->getStatus($id);
+            $_res['current_count'] = self::$tournamentModel->getCurrentCount($id);
+            $_res['type'] = self::$tournamentModel->getTournamentType($id);
+            $res[$i] = $_res;
+            $i++;
+        }
+        echo json_encode($res);
+
+    }
+
+    ///init/tournament
+    public static function initTournament(){
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+
+        $id = $data['id'];
+        self::$tournamentModel->setStatus($id, "On Going");
+        self::$tournamentModel->initLadderTournament($id);
+        
+        echo json_encode([
+            'status' => 'success'
+        ]);
+    }
+    ///view/tournamentDetails/{id:\d+}
+    public static function viewDetails($id){
+        $id = $id['id'];
+        {
+            "name":"",
+            "currrentMatch":'',
+            "totalMatch":'',
+            "matches":[
+                {
+                    "match_no" :''
+                    "name": ""
+                    "points": ""
+
+                }
+            ],
+        }
+
+        
+
+    }
+    ///update/scores
+    public static function updateScores(){
 
     }
 
